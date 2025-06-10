81f4a1712209e6c8db6ef4ed71878560a161e8f9
sarkis <sarkis02@list.ru>
2025-06-10 19:14:04 +0300

    create todo and weather app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,188 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import ToDo from "./Tasks";
+import ToDoForm from "./AddTask";
+
+const TASKS_STORAGE_KEY = "tasks-list-project-web";
+const weatherApiKey = "c7616da4b68205c2f3ae73df2c31d177";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [rates, setRates] = useState({});
+  const [weatherData, setWeatherData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  // Восстановление задач из LocalStorage
+  const [todos, setTodos] = useState(() => {
+    const storedTasks = localStorage.getItem(TASKS_STORAGE_KEY); // Читает задачи из хранилища по ключу
+    if (storedTasks) {
+      try {
+        const parsedTasks = JSON.parse(storedTasks); // Парсит JSON с задачами
+        if (Array.isArray(parsedTasks)) {
+          return parsedTasks; // Возвращает задачи
+        } else {
+          console.warn(
+            "Задача была найдена, но имеет неправильное содержимое:",
+            parsedTasks
+          );
+        }
+      } catch (error) {
+        console.error(
+          "Ошибка при чтении задач из localStorage:",
+          error.message
+        );
+      }
+    }
+    return []; // Возвращает пустой массив, если нет задач
+  });
+
+  useEffect(() => {
+    async function fetchAllData() {
+      try {
+        const currencyResponse = await axios.get(
+          "https://www.cbr-xml-daily.ru/daily_json.js"
+        );
+
+        if (!currencyResponse.data || !currencyResponse.data.Valute) {
+          throw new Error("Нет данных о валюте.");
+        }
+        const USDrate = currencyResponse.data.Valute.USD.Value.toFixed(
+          4
+        ).replace(".", ",");
+        const EURrate = currencyResponse.data.Valute.EUR.Value.toFixed(
+          4
+        ).replace(".", ",");
+        setRates({
+          USDrate,
+          EURrate,
+        });
+        navigator.geolocation.getCurrentPosition(
+          async (position) => {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+            const weatherResponse = await axios.get(
+              `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherApiKey}`
+            );
+
+            if (!weatherResponse.data.main) {
+              throw new Error("Нет данных о погоде.");
+            }
+
+            setWeatherData(weatherResponse.data);
+          },
+          (error) => {
+            console.error(error);
+          }
+        );
+      } catch (err) {
+        console.error(err);
+        setError("Ошибка загрузки данных.");
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchAllData();
+  }, []);
+
+  // Автосохранение задач в LocalStorage
+  useEffect(() => {
+    try {
+      localStorage.setItem(TASKS_STORAGE_KEY, JSON.stringify(todos)); // Сохранение задач в формате JSON
+    } catch (error) {
+      console.error(
+        "Ошибка при сохранении задач в localStorage:",
+        error.message
+      );
+    }
+  }, [todos]); // срабатывает при изменениях в tasks
+
+  const addTask = (userInput) => {
+    if (userInput) {
+      const newItem = {
+        id: Math.random().toString(36).substr(2, 9),
+        task: userInput,
+        complete: false,
+      };
+      setTodos([...todos, newItem]);
+    }
+  };
+
+  const removeTask = (id) => {
+    setTodos([...todos.filter((todo) => todo.id !== id)]);
+  };
+
+  const handleToggle = (id) => {
+    setTodos([
+      ...todos.map((task) =>
+        task.id === id ? { ...task, complete: !task.complete } : { ...task }
+      ),
+    ]);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        {/* // Проверяем состояние загрузки (loading): при loading === true,
+        отображается сообщение "Загрузка..." */}
+        {loading && <p>Загрузка...</p>}
+        {/* // Если загрузка завершилась (!loading), проверяем наличие ошибки
+        (error), в случае, если ошибка присутствует, выводим её красным цветом. */}
+        {!loading && error && <p style={{ color: "red" }}>{error}</p>}
+        {/* // Если ошибки нет, выводится информация, полученная из api */}
+        {!loading && !error && (
+          <>
+            <div className="info">
+              {/* // Блок money с текущими курсами USD и EUR */}
+              <div className="money">
+                <div id="USD">Доллар США $ — {rates.USDrate} руб.</div>
+                <div id="EUR">Евро € — {rates.EURrate} руб.</div>
+              </div>
+              {/* // Если есть данные о погоде (weatherData), показываем блок с
+              погодой: данные о температуре воздуха в переводе на градусы по
+              Цельсию, скорость ветра, процент облачности и иконку погоды с
+              изображением */}
+              {weatherData && (
+                <div className="weather-info">
+                  <div>
+                    Погода сегодня: <br></br> 🌡️{" "}
+                    {(weatherData.main.temp - 273.15).toFixed(1)}°C ༄.°{" "}
+                    {weatherData.wind.speed} м/с ☁️ {weatherData.clouds.all}%
+                  </div>
+                  <img
+                    className="weather-icon"
+                    src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`}
+                    alt="Иконка погоды"
+                  />
+                </div>
+              )}
+            </div>
+          </>
+        )}
+        {/* // Заголовочная секция header с заголовком списка задач и количеством 
+        активных дел*/}
+        <header>
+          <h1 className="list-header">Список задач: {todos.length}</h1>
+        </header>
+        {/* // Форма добавления задач ToDoForm, передающая метод addTask в качестве
+        пропса */}
+        <ToDoForm addTask={addTask} />
+        {/* // Компоненты задач Todo, создаваемые циклом map для каждого элемента
+        массива todos */}
+        {todos.map((todo) => {
+          return (
+            <ToDo
+              todo={todo}
+              key={todo.id}
+              toggleTask={handleToggle}
+              removeTask={removeTask}
+            />
+          );
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
